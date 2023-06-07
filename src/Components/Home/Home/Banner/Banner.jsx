e45b05ee019ee27244f40a272ec4823e8e9821c5
Shaik Abdul Ahad <118795198+ShaikAbdul01@users.noreply.github.com>
2023-06-07 13:42:58 +0600

    Banner section responsive

--- src/Components/Home/Home/Banner/Banner.jsx
+++ src/Components/Home/Home/Banner/Banner.jsx
@@ -1,8 +1,8 @@
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import bg1 from "../../../../assets/bg/1.jpeg";
-import bg2 from "../../../../assets/bg/2.avif";
-import bg3 from "../../../../assets/bg/3.avif";
+import bg1 from "../../../../assets/bg/1.jpg";
+import bg2 from "../../../../assets/bg/2.jpg";
+import bg3 from "../../../../assets/bg/3.jpg";
 
 import "./Banner.css";
 
@@ -15,29 +15,42 @@ const Banner = () => {
       infiniteLoop={true}
       autoPlay={true}
       interval={3000}
+      className="w-full"
     >
-      <div className="banner-slide h-[700px]">
-        <img src={bg1} alt="Slide 1" />
+      <div className="banner-slide h-[400px] md:h-[500px] lg:h-[700px]">
+        <img src={bg1} alt="Slide 1" className="w-full h-full object-cover" />
         <div className="banner-overlay">
-          <h2>Welcome to Artistry Academy</h2>
-          <p>Unleash your creativity and explore the world of art.</p>
-          <button className="banner-button">Learn More</button>
+          <h2 className="text-3xl md:text-4xl lg:text-5xl">
+            Welcome to Artistry Academy
+          </h2>
+          <p className="text-xl md:text-2xl lg:text-3xl">
+            Unleash your creativity and explore the world of art.
+          </p>
+          <button className="banner-button text-xl">Learn More</button>
         </div>
       </div>
-      <div className="banner-slide">
-        <img src={bg2} alt="Slide 2" />
+      <div className="banner-slide h-[400px] md:h-[500px] lg:h-[700px]">
+        <img src={bg2} alt="Slide 2" className="w-full h-full object-cover" />
         <div className="banner-overlay">
-          <h2>Discover Your Artistic Passion</h2>
-          <p>Join our diverse community of artists and express yourself.</p>
-          <button className="banner-button">Get Started</button>
+          <h2 className="text-3xl md:text-4xl lg:text-5xl">
+            Discover Your Artistic Passion
+          </h2>
+          <p className="text-xl md:text-2xl lg:text-3xl">
+            Join our diverse community of artists and express yourself.
+          </p>
+          <button className="banner-button text-xl">Get Started</button>
         </div>
       </div>
-      <div className="banner-slide">
-        <img src={bg3} alt="Slide 3" />
+      <div className="banner-slide h-[400px] md:h-[500px] lg:h-[700px]">
+        <img src={bg3} alt="Slide 3" className="w-full h-full object-cover" />
         <div className="banner-overlay">
-          <h2>Unlock Your Creative Potential</h2>
-          <p>Experience innovative art classes and workshops.</p>
-          <button className="banner-button">Enroll Now</button>
+          <h2 className="text-3xl md:text-4xl lg:text-5xl">
+            Unlock Your Creative Potential
+          </h2>
+          <p className="text-xl md:text-2xl lg:text-3xl">
+            Experience innovative art classes and workshops.
+          </p>
+          <button className="banner-button text-xl">Enroll Now</button>
         </div>
       </div>
     </Carousel>
