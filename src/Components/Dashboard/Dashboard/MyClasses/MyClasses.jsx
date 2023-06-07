e3d21ba71330c3655914d81ad466ee5d27d00632
Shaik Abdul Ahad <118795198+ShaikAbdul01@users.noreply.github.com>
2023-06-08 01:54:29 +0600

    students deshboart table showing and delete button implement

--- src/Components/Dashboard/Dashboard/MyClasses/MyClasses.jsx
+++ src/Components/Dashboard/Dashboard/MyClasses/MyClasses.jsx
@@ -1,22 +1,89 @@
+import { FaTrashAlt } from "react-icons/fa";
 import { useClassItem } from "../../../UseHooks/UseClassItem";
+import Swal from "sweetalert2";
 
 const MyClasses = () => {
-  const { classes, isLoading } = useClassItem();
+  const { classes, isLoading, refetch } = useClassItem();
 
   if (isLoading) {
     return <div>Loading...</div>;
   }
 
-  console.log(classes);
+  const handleDelete = (classItemId) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/classItem/${classItemId}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount > 0) {
+              refetch();
+              Swal.fire("Deleted!", "Your file has been deleted.", "success");
+            }
+          });
+      }
+    });
+  };
+
   return (
     <div>
-      <h3 className="text-4xl">Classes {classes.length}</h3>
-      {classes.map((classItem) => (
-        <div key={classItem.id}>
-          <h4>{classItem.title}</h4>
-          {/* Render other class item details */}
-        </div>
-      ))}
+      <h3 className="text-4xl">My Selected Classes</h3>
+      <div className="overflow-x-auto w-full">
+        <table className="table w-full">
+          {/* Head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Image</th>
+              <th>Name</th>
+              <th>Instructor</th>
+              <th>Available Seats</th>
+              <th>Price</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {classes?.map((item, index) => (
+              <tr key={item._id}>
+                <td>{index + 1}</td>
+                <td>
+                  <div className="avatar">
+                    <div className="mask mask-squircle w-12 h-12">
+                      <img src={item.image} alt={item.name} />
+                    </div>
+                  </div>
+                </td>
+                <td>{item.name}</td>
+                <td>{item.instructorName}</td>
+                <td>{item.availableSeats}</td>
+                <td>${item.price}</td>
+                <td>
+                  <div className="flex justify-center">
+                    <button
+                      onClick={() => handleDelete(item._id)}
+                      className="btn btn-ghost btn-xs bg-red-500 text-white mr-2"
+                    >
+                      <FaTrashAlt />
+                    </button>
+                    <button className="btn bg-blue-400 text-white btn-xs">
+                      Pay Now
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
